c5bd08ec75d8881bf888a4c546b3a79fa20af365
Ozan Cankal <44276250+skrimenos@users.noreply.github.com>
2023-04-10 23:00:57 +0300

    Web Api Created

---
+++ src/WebApi/OnionArchitecture.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Application.Dto;
+using OnionArchitecture.Application.Interfaces.Repository;
+
+namespace OnionArchitecture.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allList = await productRepository.GetAllAsync();
+
+            //wrong way
+            var result = allList.Select(x => new ProductViewDto { Id = x.Id, Name = x.Name });
+
+            return Ok(result);
+        }
+    }
+}
