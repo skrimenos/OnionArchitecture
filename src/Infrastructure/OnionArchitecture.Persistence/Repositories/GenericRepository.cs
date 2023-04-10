ec8d57b06df3f935e3233da22c6635d9fb558bc0
Ozan Cankal <44276250+skrimenos@users.noreply.github.com>
2023-04-10 22:37:25 +0300

    Infrastructure Created

---
+++ src/Infrastructure/OnionArchitecture.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Application.Interfaces.Repository;
+using OnionArchitecture.Domain.Common;
+using OnionArchitecture.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepositoryAsync<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext dbContext;
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await dbContext.Set<T>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
---
+++ src/Infrastructure/OnionArchitecture.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnionArchitecture.Application.Interfaces.Repository;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
